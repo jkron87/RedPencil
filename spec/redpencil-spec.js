5c96e0affe593ec67e20a4bc27560cdf542af064
willeml85 <willeml85@gmail.com>
2016-03-10 20:56:12 -0500

    RedPencil 0.1
    Created project.
    Added function GetProductIndexById() and first test.
    Test fails.

---
+++ spec/redpencil-spec.js
@@ -0,0 +1,8 @@
+describe("Modify Product Price", function () {
+
+    it("returns index 1 when product id \"B5\" is passed", function () {
+        var index = GetProductIndexById("B5");
+        expect(index).toBe(1);
+    });
+
+});
